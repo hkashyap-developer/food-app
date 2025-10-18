0b467f369b7d744788bccba03338e423ea108e98
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2025-10-18 18:21:19 +0530

    Stable

--- components/custom/hero-banner/hero-banner.tsx
+++ components/custom/hero-banner/hero-banner.tsx
@@ -19,11 +19,15 @@ const Herobanner = (props: {
   const { title, description, buttonOne, buttonTwo, coverImage } = props;
 
   return (
-    <section className="w-full bg-gray-50 py-0 pb-10 md:py-20 border-b-1">
-      <div className="container mx-auto px-0 sm:px-4 flex justify-between flex-col-reverse md:flex-row items-center gap-0">
+    <section className="w-full bg-gray-50 py-0 pb-10 sm:py-16 md:py-20 border-b-1">
+      <div className="container mx-auto px-0 sm:px-4 flex justify-between flex-col-reverse md:flex-row items-center gap-4 md:gap-8">
         {/* Text Section */}
         <div className="flex-1 text-center md:text-left space-y-6 px-4 pt-8 sm:px-0 max-w-[420px] sm:max-w-[720px]">
-          {title && <h1 className="text-3xl md:text-7xl font-bold">{title}</h1>}
+          {title && (
+            <h1 className="text-3xl md:text-4xl xl:text-6xl 2xl:text-7xl font-bold">
+              {title}
+            </h1>
+          )}
           {description && (
             <p className="text-muted-foreground text-lg md:text-xl max-w-xl mx-auto md:mx-0">
               {description}
