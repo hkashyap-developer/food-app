62ce8146ae9766fe3633eff722437efaff50ff47
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2025-10-24 03:10:24 +0530

    Stable

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,21 +1,27 @@
 import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId, studioUrl } from "@/sanity/lib/api";
-
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: false,
+  projectId:
+    process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
+    process.env.SANITY_API_PROJECT_ID ||
+    process.env.SANITY_STUDIO_PROJECT_ID,
+  dataset:
+    process.env.NEXT_PUBLIC_SANITY_DATASET ||
+    process.env.SANITY_API_DATASET ||
+    "production",
+  apiVersion: "2025-10-24",
+  token:
+    process.env.SANITY_API_READ_TOKEN ||
+    process.env.SANITY_API_WRITE_TOKEN, // token for build-time / SSR
+  useCdn: false, // must be false when using token
   perspective: "published",
   stega: {
-    studioUrl,
+    studioUrl: "/studio",
     logger: console,
     filter: (props) => {
       if (props.sourcePath.at(-1) === "title") {
         return true;
       }
-
       return props.filterDefault(props);
     },
   },
